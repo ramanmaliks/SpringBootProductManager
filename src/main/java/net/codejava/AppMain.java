cdee80a5454b5c221c77249c04e98441f2814637
Raman Malik <88385518+ramanmaliks@users.noreply.github.com>
2022-06-06 16:35:24 +0530

    Update for WAR

--- src/main/java/net/codejava/AppMain.java
+++ src/main/java/net/codejava/AppMain.java
@@ -4,7 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppMain {
+public class AppMain extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppMain.class);
+		
 	public static void main(String[] args) {
 		SpringApplication.run(AppMain.class, args);
 	}
